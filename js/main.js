517a817876560d25991c3b72fd98b45d9cd5433d
SAMORN.MAI <SAMORN.MAI@PNC.LAN>
2023-12-18 09:50:13 +0700

    #39 fix history

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function date() {
     document.getElementById("month-new").innerHTML = name;
     let year_new = d.getFullYear();
     document.querySelector("#year-new").innerHTML = year_new;
-    document.querySelector("#day-old").innerHTML = 1-day_new + day_new;
+    document.querySelector("#day-old").innerHTML = 1 - day_new + day_new;
     const month_old = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let name_old = month_old[d.getMonth()];
     document.getElementById("month-old").innerHTML = name_old;
@@ -40,11 +40,41 @@ const searchMovieTitle = () => {
         } else {
             tr1.parentElement.style.display = 'none';
         }
-
     }
 }
 const searchText = document.querySelector('#search');
 searchText.addEventListener('keyup', searchMovieTitle);
+//.......table..........
+let Tbody = document.querySelector("tbody");
+let trOftbody = document.createElement("tr");
+trOftbody.style.padding = "10px";
+let tdOfId = document.createElement("td");
+tdOfId.textContent = "#12";
+trOftbody.appendChild(tdOfId);
+
+let tdOfName = document.createElement("td");
+tdOfName.textContent = "Sovannarith";
+trOftbody.appendChild(tdOfName);
+
+let tdOfDate = document.createElement("td");
+tdOfDate.textContent = "12 / Dec / 2023";
+trOftbody.appendChild(tdOfDate);
+
+let tdOfTotal = document.createElement("td");
+tdOfTotal.textContent = "100$";
+trOftbody.appendChild(tdOfTotal);
+
+let tdOfAction = document.createElement("td");
+let button = document.createElement("button");
+button.textContent = "Delete";
+button.style.backgroundColor = "red";
+button.style.color = 'white';
+button.style.padding = '5px 10px';
+button.style.border = 'none';
+button.style.borderRadius = '4px';
+tdOfAction.appendChild(button);
+trOftbody.appendChild(tdOfAction);
+Tbody.append(trOftbody)
 
 
 
